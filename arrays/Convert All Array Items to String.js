420de10f7a55bd2c093333ebdccb2d9d34a556b5
Sami-Tak/ <samitak7875@gmail.com>
2026-01-20 11:57:40 +0530

    pushed from new2_branch

--- arrays/Convert All Array Items to String.js
+++ arrays/Convert All Array Items to String.js
@@ -5,6 +5,6 @@ function parseArray(arr) {
 }
 
 console.log(parseArray([1, 2, "a", "b"]));
-console.log(parseArray(["abc", 123, "def", 456]));
-console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]));
-console.log(parseArray([]));
+// console.log(parseArray(["abc", 123, "def", 456]));
+// console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]));
+// console.log(parseArray([]));
