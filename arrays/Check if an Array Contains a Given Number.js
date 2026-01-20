6be4f239a02a760e189b2f04b0e044cb2b43a4d6
Sami-Tak/ <samitak7875@gmail.com>
2026-01-20 12:47:25 +0530

    pushed from new6_branch

--- arrays/Check if an Array Contains a Given Number.js
+++ arrays/Check if an Array Contains a Given Number.js
@@ -44,11 +44,11 @@
 // console.log(check([5, 5, 5, 6], 5));
 // console.log(check([], 5));
 
-// function check(arr, el) {
-    // return arr.some(value => value === el); 
-// }
+function check(arr, el) {
+    return arr.some(value => value === el); 
+}
 
-// console.log(check([1, 2, 3, 4, 5], 3));
-// console.log(check([1, 1, 2, 1, 1], 3));
-// console.log(check([5, 5, 5, 6], 5));
-// console.log(check([], 5));
+console.log(check([1, 2, 3, 4, 5], 3));
+console.log(check([1, 1, 2, 1, 1], 3));
+console.log(check([5, 5, 5, 6], 5));
+console.log(check([], 5));
